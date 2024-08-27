dd664f5ebcc3ea5540e2084ac4ea907e6b0db207
Artem <29069662+eadium@users.noreply.github.com>
2024-08-27 21:54:54 +0100

    some old config changes

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1793,8 +1793,8 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-//#define MINIMUM_STEPPER_POST_DIR_DELAY 650
-//#define MINIMUM_STEPPER_PRE_DIR_DELAY 650
+#define MINIMUM_STEPPER_POST_DIR_DELAY 20
+#define MINIMUM_STEPPER_PRE_DIR_DELAY 20
 
 /**
  * Minimum stepper driver pulse width (in µs)
@@ -1821,7 +1821,7 @@
  *
  * Override the default value based on the driver type set in Configuration.h.
  */
-//#define MAXIMUM_STEPPER_RATE 250000
+#define MAXIMUM_STEPPER_RATE 5000000
 
 // @section temperature
 
